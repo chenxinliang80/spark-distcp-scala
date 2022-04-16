037f6bf63d2bbc0c42ac1a2a992bd77e064b7b61
2232186589@qq.com <yang.kai@bngrp.cn>
2022-04-16 21:04:29 +0800

    Initial commit

---
+++ src/test/scala/com/sparkdata/TestSparkDistCPOptions.scala
@@ -0,0 +1,19 @@
+package com.sparkdata
+
+import com.sparkdata.SparkDistCPOptions
+import org.scalatest.{FunSpec, Matchers}
+
+class TestSparkDistCPOptions extends FunSpec with Matchers {
+
+  it("updateOverwritePathBehaviour"){
+
+    SparkDistCPOptions().updateOverwritePathBehaviour should be (false)
+    SparkDistCPOptions(consistentPathBehaviour = true).updateOverwritePathBehaviour should be (false)
+    SparkDistCPOptions(update = true, consistentPathBehaviour = true).updateOverwritePathBehaviour should be (false)
+    SparkDistCPOptions(overwrite = true, consistentPathBehaviour = true).updateOverwritePathBehaviour should be (false)
+    SparkDistCPOptions(update = true).updateOverwritePathBehaviour should be (true)
+    SparkDistCPOptions(overwrite = true).updateOverwritePathBehaviour should be (true)
+
+  }
+
+}
